2444fdb695656633466aa4bd86157cb90f9317f8
Thomas Perrin <thomas.perrin@drotek.com>
2022-06-22 12:01:11 +0200

    archir src refonte for pytest

--- test/procedure_test/show_check_test/drone_check_test/events_format_check/events_format_check_procedure_test.py
+++ test/procedure_test/show_check_test/drone_check_test/events_format_check/events_format_check_procedure_test.py
@@ -1,3 +1,17 @@
+import pytest
+
+# from ......procedure.show_check.drone_check.events_format_check.events_format_check_procedure import (
+#     apply_events_format_check_procedure,
+# )
+from .....procedure_test.show_check_test.family_check_test.family_check_procedure_test import (
+    papa,
+)
+
+
 @pytest.fixture
 def my_position_events():
-def test_position_events_check()
+    return 0
+
+
+def test_position_events_check():
+    return 0
