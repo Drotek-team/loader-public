c6dd5bfef155360759ae87bb37f70941c7f71d61
Thomas PERRIN <thomas.perrin@drotek.com>
2022-07-27 14:16:39 +0200

    DanceSizeCheckReport => Contenor

--- src/procedure/show_check/drone_check/dance_size_check/dances_size_check_report.py
+++ src/procedure/show_check/drone_check/dance_size_check/dances_size_check_report.py
@@ -1,9 +1,10 @@
 from ....json_conversion.json_convertion_tools.drone_encoding_report import (
     DroneEncodingReport,
 )
+from ....report import Contenor
 
 
-class DanceSizeCheckReport:
+class DanceSizeCheckReport(Contenor):
     def __init__(self):
-        self.validation = False
+        self.name = f"Dance size check report"
         self.drone_encoding_report = DroneEncodingReport()
