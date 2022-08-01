085e2647871d95161f91164964ee872d3845f457
Thomas PERRIN <thomas.perrin@drotek.com>
2022-08-01 12:24:33 +0200

    Replace example dance with correct one

--- json_manager.py
+++ json_manager.py
@@ -1,11 +1,15 @@
 from dance_test_json import JSON_EXAMPLE
 from src.procedure.import_procedure import apply_import_procedure
 from src.procedure.import_report import ImportReport
+from src.parameter.parameter import Parameter
+import os
 
 
 def main() -> None:
     import_report = ImportReport()
-    apply_import_procedure(JSON_EXAMPLE, import_report)
+    parameter = Parameter()
+    parameter.load_parameter(os.getcwd())
+    apply_import_procedure(JSON_EXAMPLE, import_report, parameter)
     print(import_report.get_contenor_report(0, "   "))
 
 
