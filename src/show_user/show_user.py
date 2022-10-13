bf2c66c3ee3042d3497a295e16f2275136002d65
Thomas PERRIN <thomas.perrin@drotek.com>
2022-10-13 14:45:08 +0200

    show user new format

--- src/show_user/show_user.py
+++ src/show_user/show_user.py
@@ -1,7 +1,31 @@
 from pydantic import BaseModel
-from typing import List
-from .drone_user.drone_user import DroneUser
+from typing import List, Tuple
 import json
+
+
+class PositionEventUser(BaseModel):
+    position_frame: int  # 4 frame per second
+    absolute_frame: int  # 24 frame per second
+    xyz: Tuple[float, float, float]  # ENU and meter
+
+
+class ColorEventUser(BaseModel):
+    color_frame: int  # 24 frame per second
+    absolute_frame: int  # 24 frame per second
+    rgbw: Tuple[float, float, float, float]  # between 0 and 1
+
+
+class FireEventUser(BaseModel):
+    fire_frame: int  # 24 frame per second
+    absolute_frame: int  # 24 frame per second
+    chanel: float  # Chanel of the drone
+    duration: float  # Duration of the event
+
+
+class DroneUser(BaseModel):
+    position_events: List[PositionEventUser]
+    color_events: List[ColorEventUser]
+    fire_events: List[FireEventUser]
 
 
 class ShowUser(BaseModel):
