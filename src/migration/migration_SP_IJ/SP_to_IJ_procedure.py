205d484dc4c3b7bfcaf9ae84f5310d574235f59d
Thomas Perrin <thomas.perrin@drotek.com>
2023-01-09 16:04:49 +0100

    naming omission

--- src/migration/migration_SP_IJ/SP_to_IJ_procedure.py
+++ src/migration/migration_SP_IJ/SP_to_IJ_procedure.py
@@ -3,7 +3,7 @@ from ...show_px4.show_px4 import ShowPx4
 from ..migration_DP_binary.drone_encoding_procedure import encode_drone
 
 
-def SP_to_IJG_procedure(show_px4: ShowPx4) -> IostarJson:
+def SP_to_IJ_procedure(show_px4: ShowPx4) -> IostarJson:
     return IostarJson(
         **{
             "binary_dances": [encode_drone(drone_px4) for drone_px4 in show_px4],
--- src/procedure/export_procedure.py
+++ src/procedure/export_procedure.py
@@ -5,7 +5,7 @@ from ..check.all_check_from_show_user_procedure import (
 )
 from ..check.show_check_report import ShowCheckReport
 from ..iostar_json.iostar_json import IostarJson
-from ..migration.migration_SP_IJG.SP_to_IJG_procedure import SP_to_IJG_procedure
+from ..migration.migration_SP_IJ.SP_to_IJ_procedure import SP_to_IJ_procedure
 from ..migration.migration_SP_SU.SU_to_SP_procedure import SU_to_SP_procedure
 from ..show_user.show_user import ShowUser
 
@@ -17,6 +17,6 @@ def apply_export_procedure(
     show_check_report = ShowCheckReport(len(show_user.drones_user))
     apply_all_check_from_show_user_procedure(show_user, show_check_report)
     return (
-        SP_to_IJG_procedure(SU_to_SP_procedure(show_user)),
+        SP_to_IJ_procedure(SU_to_SP_procedure(show_user)),
         show_check_report,
     )
