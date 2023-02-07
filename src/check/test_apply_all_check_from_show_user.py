a6c3d01160bd0152133ae6a96c8617172d135dbb
Thomas Perrin <thomas.perrin@drotek.com>
2023-02-07 16:12:24 +0100

    update tolerance 4 frame takeoff duration

--- src/check/test_apply_all_check_from_show_user.py
+++ src/check/test_apply_all_check_from_show_user.py
@@ -11,16 +11,16 @@ def test_apply_all_check_from_show_user_standard_case():
     assert all_check_contenor.user_validation
 
 
-def test_apply_all_check_from_show_user_show_user_check_one_frame_tolerance():
+def test_apply_all_check_from_show_user_show_user_check_four_frame_tolerance():
     valid_show_user = get_valid_show_user(ShowUserConfiguration(nb_x=2, nb_y=2))
-    valid_show_user[0].position_events[0].frame = 1
+    valid_show_user[0].position_events[0].frame = 4
     all_check_contenor = apply_all_check_from_show_user(valid_show_user)
     assert all_check_contenor.user_validation
 
 
 def test_apply_all_check_from_show_user_incorrect_show_user_check():
     valid_show_user = get_valid_show_user(ShowUserConfiguration(nb_x=2, nb_y=2))
-    valid_show_user[0].position_events[0].frame = 2
+    valid_show_user[0].position_events[0].frame = 5
     all_check_contenor = apply_all_check_from_show_user(valid_show_user)
     assert not (all_check_contenor.user_validation)
 
