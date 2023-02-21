ad430e068625e393db2047b384ddaa1b4e26ebf7
Jonathan Plasse <jonathan.plasse@drotek.com>
2023-02-21 10:25:38 +0100

    fix: remove try except ImportError, ModuleNotFoundError
    It is not needed anymore as we never call loader relatively

--- loader/__init__.py
+++ loader/__init__.py
@@ -1,34 +1,30 @@
-from contextlib import suppress
-
-with suppress(ImportError):
-    from .editor import (
-        CollisionInfraction,
-        DanceSizeInfraction,
-        DroneUser,
-        GlobalReport,
-        IostarJsonGcs,
-        PerformanceInfraction,
-        PerformanceKind,
-        PerformanceRange,
-        ReportError,
-        ShowConfigurationGcs,
-        ShowPositionFrames,
-        ShowUser,
-        convert_iostar_json_gcs_string_to_show_user,
-        convert_show_user_to_iostar_json_gcs,
-        create_empty_show_user,
-        create_show_position_frames_from_frames_positions,
-        create_show_position_frames_from_show_user,
-        generate_report_from_iostar_json_gcs_string,
-        generate_report_from_show_user,
-        get_collision_infractions,
-        get_dance_size_infractions,
-        get_performance_infractions,
-        get_show_configuration_from_iostar_json_gcs_string,
-        get_verified_iostar_json_gcs,
-    )
-    from .report.global_report import GlobalReportSummary
-
+from .editor import (
+    CollisionInfraction,
+    DanceSizeInfraction,
+    DroneUser,
+    GlobalReport,
+    IostarJsonGcs,
+    PerformanceInfraction,
+    PerformanceKind,
+    PerformanceRange,
+    ReportError,
+    ShowConfigurationGcs,
+    ShowPositionFrames,
+    ShowUser,
+    convert_iostar_json_gcs_string_to_show_user,
+    convert_show_user_to_iostar_json_gcs,
+    create_empty_show_user,
+    create_show_position_frames_from_frames_positions,
+    create_show_position_frames_from_show_user,
+    generate_report_from_iostar_json_gcs_string,
+    generate_report_from_show_user,
+    get_collision_infractions,
+    get_dance_size_infractions,
+    get_performance_infractions,
+    get_show_configuration_from_iostar_json_gcs_string,
+    get_verified_iostar_json_gcs,
+)
+from .report.global_report import GlobalReportSummary
 
 __all__ = (
     "CollisionInfraction",
