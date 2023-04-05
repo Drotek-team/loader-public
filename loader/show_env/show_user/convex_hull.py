67f7114800f2f58b8483fc7cb3bb58fbc43eae9f
Jonathan Plasse <jonathan.plasse@drotek.com>
2023-04-05 17:39:59 +0200

    style(pyright): use TypeVar for position tuples

--- loader/show_env/show_user/convex_hull.py
+++ loader/show_env/show_user/convex_hull.py
@@ -1,17 +1,19 @@
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, TypeVar
+
+TPosition = TypeVar("TPosition", Tuple[float, float], Tuple[int, int])
 
 
 class AngleOutOfBoundsError(Exception):
     pass
 
 
-def get_p0(positions: Iterable[Tuple[float, float]]) -> Tuple[float, float]:
+def get_p0(positions: Iterable[TPosition]) -> TPosition:
     return min(positions, key=lambda p: (p[1], p[0]))
 
 
 def calculate_angle(
-    position0: Tuple[float, float],
-    position: Tuple[float, float],
+    position0: TPosition,
+    position: TPosition,
 ) -> Tuple[float, float]:
     x = position[0] - position0[0]
     y = position[1] - position0[1]
@@ -22,24 +24,24 @@ def calculate_angle(
     raise AngleOutOfBoundsError
 
 
-def sort_positions(positions: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
+def sort_positions(positions: List[TPosition]) -> List[TPosition]:
     p0 = get_p0(positions)
     return sorted(positions, key=lambda p: calculate_angle(p0, p))
 
 
 def cross_product(
-    p0: Tuple[float, float],
-    p1: Tuple[float, float],
-    p2: Tuple[float, float],
+    p0: TPosition,
+    p1: TPosition,
+    p2: TPosition,
 ) -> float:
     return (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p1[1] - p0[1]) * (p2[0] - p0[0])
 
 
 def calculate_convex_hull(
-    positions: List[Tuple[float, float]],
-) -> List[Tuple[float, float]]:
+    positions: List[TPosition],
+) -> List[TPosition]:
     sorted_positions = sort_positions(positions)
-    stack: List[Tuple[float, float]] = []
+    stack: List[TPosition] = []
 
     for p in sorted_positions:
         while len(stack) > 1 and cross_product(stack[-1], stack[-2], p) >= 0:
