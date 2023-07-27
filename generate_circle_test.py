5b7e0bb0442cfa0d76faf3d4c7fed4ba889da11f
Jonathan Plasse <jonathan.plasse@drotek.com>
2023-07-27 08:35:39 +0200

    chore: add if __name__ == "__main__"

--- generate_circle_test.py
+++ generate_circle_test.py
@@ -19,37 +19,42 @@ def to_frame(time: float) -> int:
     return FRAME_PARAMETERS.from_second_to_frame(time)
 
 
-show_user = ShowUser.create(nb_drones=1, angle_takeoff=0, step=1)
+if __name__ == "__main__":
+    show_user = ShowUser.create(nb_drones=1, angle_takeoff=0, step=1)
 
-drone = show_user.drones_user[0]
+    drone = show_user.drones_user[0]
 
-time = 0
-position = np.zeros(3)
-drone.add_position_event(time, tuple(position))
-drone.add_color_event(time, (0, 0, 0, 1))
+    time = 0
+    position = np.zeros(3)
+    drone.add_position_event(time, tuple(position))
+    drone.add_color_event(time, (0, 0, 0, 1))
 
-time = 10
-position[2] = 2
-drone.add_position_event(to_frame(time), tuple(position))
+    time = 10
+    position[2] = 2
+    drone.add_position_event(to_frame(time), tuple(position))
 
-for _ in tqdm(range(round(DURATION / CIRCLE_REVOLUTION_TIME)), desc="Draw circles", unit="circle"):
-    for angle in tqdm(
-        np.linspace(0, 2 * np.pi, NB_POINTS_ON_CIRCLE, endpoint=False),
-        desc="Draw circle",
-        unit="point",
-        leave=False,
+    for _ in tqdm(
+        range(round(DURATION / CIRCLE_REVOLUTION_TIME)),
+        desc="Draw circles",
+        unit="circle",
     ):
-        time += CIRCLE_REVOLUTION_TIME / NB_POINTS_ON_CIRCLE
-        x = RADIUS * np.cos(angle)
-        y = RADIUS * np.sin(angle)
-        z = ALT_OSCILLATION * np.sin(angle) + ALTITUDE
-        position = np.array([x, y, z])
-        drone.add_position_event(to_frame(time), tuple(position))
+        for angle in tqdm(
+            np.linspace(0, 2 * np.pi, NB_POINTS_ON_CIRCLE, endpoint=False),
+            desc="Draw circle",
+            unit="point",
+            leave=False,
+        ):
+            time += CIRCLE_REVOLUTION_TIME / NB_POINTS_ON_CIRCLE
+            x = RADIUS * np.cos(angle)
+            y = RADIUS * np.sin(angle)
+            z = ALT_OSCILLATION * np.sin(angle) + ALTITUDE
+            position = np.array([x, y, z])
+            drone.add_position_event(to_frame(time), tuple(position))
 
-report = GlobalReport.generate(show_user, physic_parameters=IOSTAR_PHYSIC_PARAMETERS_MAX)
-if len(report):
-    print(report.summarize().model_dump_json(indent=4))  # noqa: T201
+    report = GlobalReport.generate(show_user, physic_parameters=IOSTAR_PHYSIC_PARAMETERS_MAX)
+    if len(report):
+        print(report.summarize().model_dump_json(indent=4))  # noqa: T201
 
-iostar_json_gcs = IostarJsonGcs.from_show_user(show_user)
-dance_path = Path("circle_test.json")
-dance_path.write_text(iostar_json_gcs.model_dump_json())
+    iostar_json_gcs = IostarJsonGcs.from_show_user(show_user)
+    dance_path = Path("circle_test.json")
+    dance_path.write_text(iostar_json_gcs.model_dump_json())
--- generate_up_down_test.py
+++ generate_up_down_test.py
@@ -5,11 +5,11 @@ from loader.parameters import FRAME_PARAMETERS, IOSTAR_PHYSIC_PARAMETERS_MAX
 from loader.reports import GlobalReport
 from loader.schemas import IostarJsonGcs, ShowUser
 
-DURATION = 0.69 * 60 * 60
+DURATION = 0.33 * 60 * 60
 TAKEOFF_ALTITUDE = 1
 TAKEOFF_DURATION = 10
 ACCELERATION = 1.0
-EVENT_TIME_STEP = 1 / 4
+EVENT_TIME_STEP = 1 / 8
 MAX_ALTITUDE = 5
 HALF_OSCILLATION_DURATION = np.sqrt((MAX_ALTITUDE - TAKEOFF_ALTITUDE) / ACCELERATION)
 
@@ -42,27 +42,28 @@ def calculate_altitude(time: float) -> float:
     return TAKEOFF_ALTITUDE + ACCELERATION * time**2 / 2
 
 
-show_user = ShowUser.create(nb_drones=1, angle_takeoff=0, step=1)
+if __name__ == "__main__":
+    show_user = ShowUser.create(nb_drones=1, angle_takeoff=0, step=1)
 
-drone = show_user.drones_user[0]
+    drone = show_user.drones_user[0]
 
-time = 0
-position = np.zeros(3)
-drone.add_position_event(time, tuple(position))
-drone.add_color_event(time, (1, 1, 1, 1))
+    time = 0
+    position = np.zeros(3)
+    drone.add_position_event(time, tuple(position))
+    drone.add_color_event(time, (1, 1, 1, 1))
 
-for time in np.arange(  # pyright: ignore[reportUnknownMemberType]
-    TAKEOFF_DURATION,
-    DURATION,
-    EVENT_TIME_STEP,
-):
-    position[2] = calculate_altitude(time)
-    drone.add_position_event(to_frame(time), tuple(position))
+    for time in np.arange(  # pyright: ignore[reportUnknownMemberType]
+        TAKEOFF_DURATION,
+        DURATION,
+        EVENT_TIME_STEP,
+    ):
+        position[2] = calculate_altitude(time)
+        drone.add_position_event(to_frame(time), tuple(position))
 
-report = GlobalReport.generate(show_user, physic_parameters=IOSTAR_PHYSIC_PARAMETERS_MAX)
-if len(report):
-    print(report.summarize().model_dump_json(indent=4))  # noqa: T201
+    report = GlobalReport.generate(show_user, physic_parameters=IOSTAR_PHYSIC_PARAMETERS_MAX)
+    if len(report):
+        print(report.summarize().model_dump_json(indent=4))  # noqa: T201
 
-iostar_json_gcs = IostarJsonGcs.from_show_user(show_user)
-dance_path = Path("up_down_test.json")
-dance_path.write_text(iostar_json_gcs.model_dump_json())
+    iostar_json_gcs = IostarJsonGcs.from_show_user(show_user)
+    dance_path = Path("up_down_test.json")
+    dance_path.write_text(iostar_json_gcs.model_dump_json())
