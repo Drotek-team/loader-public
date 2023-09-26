6589adec7335994ea63313980751157df8222482
Jonathan Plasse <jonathan.plasse@drotek.com>
2023-09-26 12:09:04 +0200

    perf: improve in_dance_flight_simulation() numpy use

--- loader/schemas/show_user/show_position_frame/simulation/in_dance_flight_simulation.py
+++ loader/schemas/show_user/show_position_frame/simulation/in_dance_flight_simulation.py
@@ -22,46 +22,42 @@ def in_dance_flight_simulation(
     position_events_user_z = [
         position_event_user.xyz[2] for position_event_user in position_events_user
     ]
-    desired_frames = list(
-        range(
-            position_events_user[0].frame,
-            position_events_user[-1].frame,
-        ),
+    desired_frames = np.arange(  # pyright: ignore[reportUnknownMemberType]
+        position_events_user[0].frame,
+        position_events_user[-1].frame,
     )
-    flight_positions_x = apply_decimal_number_tolerance(
-        np.interp(  # pyright: ignore[reportUnknownMemberType]
-            desired_frames,
-            position_events_user_frame,
-            position_events_user_x,
-        ),
+    flight_positions_x = np.interp(  # pyright: ignore[reportUnknownMemberType]
+        desired_frames,
+        position_events_user_frame,
+        position_events_user_x,
     )
-    flight_positions_y = apply_decimal_number_tolerance(
-        np.interp(  # pyright: ignore[reportUnknownMemberType]
-            desired_frames,
-            position_events_user_frame,
-            position_events_user_y,
-        ),
+    flight_positions_y = np.interp(  # pyright: ignore[reportUnknownMemberType]
+        desired_frames,
+        position_events_user_frame,
+        position_events_user_y,
     )
-    flight_positions_z = apply_decimal_number_tolerance(
-        np.interp(  # pyright: ignore[reportUnknownMemberType]
-            desired_frames,
-            position_events_user_frame,
-            position_events_user_z,
+    flight_positions_z = np.interp(  # pyright: ignore[reportUnknownMemberType]
+        desired_frames,
+        position_events_user_frame,
+        position_events_user_z,
+    )
+    flight_positions = apply_decimal_number_tolerance(
+        np.column_stack(  # pyright: ignore[reportUnknownMemberType]
+            (
+                flight_positions_x,
+                flight_positions_y,
+                flight_positions_z,
+            ),
         ),
     )
     return [
         SimulationInfo(
             frame=desired_frame,
-            position=np.array(
-                (flight_position_x, flight_position_y, flight_position_z),
-                dtype=np.float64,
-            ),
+            position=position,
             in_air=True,
         )
-        for desired_frame, flight_position_x, flight_position_y, flight_position_z in zip(
+        for desired_frame, position in zip(
             desired_frames,
-            flight_positions_x,
-            flight_positions_y,
-            flight_positions_z,
+            flight_positions,
         )
     ]
