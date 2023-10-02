42871c69478bd312168ca21b709344da78026ec9
Jonathan Plasse <jonathan.plasse@drotek.com>
2023-10-02 13:13:09 +0200

    docs: rework installation instructions

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,10 +1,21 @@
+from pathlib import Path
+
 import pytest
 from pytest_examples import CodeExample, EvalExample, find_examples
 
-IGNORE_PRINTS_START_LINES = {386}
+MARKDOWN_PATH = [Path("README.md")]
+
+NO_PRINTS_START_LINES = {
+    path: {
+        line_index
+        for line_index, line in enumerate(path.read_text().splitlines(), start=3)
+        if line == "<!-- no-print -->"
+    }
+    for path in MARKDOWN_PATH
+}
 
 
-@pytest.mark.parametrize("example", find_examples("README.md"), ids=str)
+@pytest.mark.parametrize("example", find_examples(*map(str, MARKDOWN_PATH)), ids=str)
 def test_readme(example: CodeExample, eval_example: EvalExample) -> None:
     eval_example.set_config(
         quotes="double",
@@ -16,13 +27,13 @@ def test_readme(example: CodeExample, eval_example: EvalExample) -> None:
     )
     if eval_example.update_examples:
         eval_example.format(example)
-        if example.start_line not in IGNORE_PRINTS_START_LINES:
+        if example.start_line not in NO_PRINTS_START_LINES[example.path]:
             eval_example.run_print_update(example)
         else:
             eval_example.run(example)
     else:
         eval_example.lint(example)
-        if example.start_line not in IGNORE_PRINTS_START_LINES:
+        if example.start_line not in NO_PRINTS_START_LINES[example.path]:
             eval_example.run_print_check(example)
         else:
             eval_example.run(example)
